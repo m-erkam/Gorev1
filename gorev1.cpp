bbdfe04a21dd0683e3e4688d36e541a4bd8267ed
m-erkam <102316645+m-erkam@users.noreply.github.com>
2022-11-07 20:38:47 +0300

    Add files via upload

---
+++ gorev1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define G 9.80665
+
+int main(){
+	float distance, time;
+	cin >> time;
+	distance = (0.5) * G * time*time;
+	cout << "Distance of the free falling object at time " << time << " is " << distance << "\n";
+	int a;
+	cin >> a;
+	
+}
